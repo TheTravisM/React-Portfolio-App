7def9e9151c568bc02d71fbb023e0a5f350e9dc9
Travis <TravisEMikolay+GitKraken@Gmail.com>
2024-08-27 19:57:01 -0400

    add Update search Pharam

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,23 +1,55 @@
-import React, { useRef } from 'react' 
-import './home.scss'
-import MikolayResume from '../../assets/Travis-Mikolay-Resume.pdf'
+import React, { useRef, useState, useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import './home.scss';
+import frontEndResume from '../../assets/docs/Travis-Mikolay-Resume.pdf';
+import reactResume from '../../assets/docs/Travis-Mikolay-Resume.pdf';
+import uxResume from '../../assets/docs/UX_UI_Travis_M_Resume.pdf';
+import engineerResume from '../../assets/docs/Engineer_Travis_M_Resume.pdf';
 import ProfileImg from '../../assets/images/ProfilePic.jpg';
 import CustomHook from '../CustomHook';
 
 const Home = () => {
   const scrollTab = useRef();
   CustomHook(scrollTab);
+
+  const [searchParams] = useSearchParams();
+  const [resumeUrl, setResumeUrl] = useState(engineerResume);
+  const [jobTitle, setJobTitle] = useState('Engineer');
+
+  useEffect(() => {
+    const title = searchParams.get('x');
+  
+    switch (title) {
+      case 'f':
+        setResumeUrl(frontEndResume);
+        setJobTitle('Front End Engineer');
+        break;
+      case 'r':
+        setResumeUrl(reactResume);
+        setJobTitle('React Engineer');
+        break;
+      case 'x':
+        setResumeUrl(uxResume);
+        setJobTitle('UX/UI Engineer');
+        break;
+      default:
+        setResumeUrl(engineerResume);
+        setJobTitle('The Engineer');
+    }
+  }, [searchParams]);
+
+
   return (
     <section id="home" ref={scrollTab}>
       <div className="content">
         <div className="name">
           <span>TRAVIS</span>
-          <br/>The Engineer
+          <br/>{jobTitle}
         </div>
         <div className="des">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias ducimus, corrupti error inventore labore reprehenderit vero laudantium tempore itaque iste. Corporis pariatur voluptate cumque officiis optio et molestias minima cum.
         </div>
-        <a className="btn-download" href={MikolayResume} target="_blank" rel="noopener noreferrer">
+        <a className="btn-download" href={resumeUrl} target="_blank" rel="noopener noreferrer">
           Download My Resume
         </a>
       </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux';
@@ -7,8 +9,10 @@ import store from './redux/store';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Router>
     <Provider store={store}>
       <App />
     </Provider>
+    </Router>
   </StrictMode>,
 )
