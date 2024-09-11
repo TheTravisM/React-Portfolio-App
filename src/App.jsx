bd64bc035820a215a9617c13cc7ef139cd033359
Travis <TravisEMikolay+GitKraken@Gmail.com>
2024-09-11 15:33:15 -0400

    Add Work Section Update text

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import NavBar from './components/NavBar/NavBar'
 import Home from './components/Home/Home'
 import Skills from './components/Skills/Skills'
+import Work from './components/Work/Work'
 import Projects from './components/Projects/Projects'
 import Contact from './components/Contact/Contact'
 
@@ -11,6 +12,7 @@ function App() {
       <NavBar />
       <Home />
       <Skills />
+      <Work />
       <Projects />
       <Contact />
     </main>
---
+++ src/components/Work/Work.jsx
@@ -0,0 +1,93 @@
+import React, { useRef } from 'react' 
+import "./work.scss";
+import workData from "./work.json";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPersonCircleQuestion,
+  faEarthAmericas,
+} from "@fortawesome/free-solid-svg-icons";
+import CustomHook from '../CustomHook';
+
+const Work = () => {
+  const divs = useRef([]);
+  const scrollTab = useRef();
+  CustomHook(scrollTab, divs);
+
+  return (
+    <section id="work" ref={scrollTab}>
+      <h2 
+        className="title work-title"
+        ref={(el) => el && divs.current.push(el)}
+      >
+        Work Portfolio
+      </h2>
+
+      <div 
+        className="description work-description"
+        ref={(el) => el && divs.current.push(el)}
+      >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis doloribus
+        quisquam quae dolor aspernatur quaerat cumque qui perferendis asperiores
+        rerum cum commodi deserunt ipsa natus earum nam, dicta dignissimos
+        perspiciatis?
+      </div>
+
+      <ul className="work-list">
+        {workData.map((value, key) => (
+          <li 
+            key={key} 
+            className="work-listItem"
+            ref={(el) => el && divs.current.push(el)}
+          >
+            <a href={value.url} 
+              className="image-wrapper"
+              target="_blank" 
+              rel="noopener noreferrer">
+              <img src={value.images} alt="" />
+            </a>
+            <div className="content">
+              <h3 className="work-listItem-title">
+                {value.name}
+              </h3>
+              <div className="work-listItem-description">
+                {value.des}
+              </div>
+              <a href={value.url} 
+                className="work-link"
+                target="_blank" 
+                rel="noopener noreferrer">
+                Link to work
+              </a>
+
+              <div className="mission">
+                <div>
+                  <FontAwesomeIcon icon={faPersonCircleQuestion} />
+                </div>
+                <div>
+                  <h4>Mission</h4>
+                  <div className="misson-description">
+                    {value.mission}
+                  </div>
+                </div>
+              </div>
+
+              <div className="mission">
+                <div>
+                  <FontAwesomeIcon icon={faEarthAmericas} />
+                </div>
+                <div>
+                  <h4>Techology</h4>
+                  <div className="misson-description">
+                    {value.technology}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Work;
